c7b28c2ab0ef1d7957bdab0f3d54bfb6fec73206
nischal1101 <kaflenk123@gmail.com>
2024-09-25 17:15:11 +0545

    fix bg color of left div

--- src/components/custom/TopingsModal.tsx
+++ src/components/custom/TopingsModal.tsx
@@ -1,7 +1,5 @@
 import { Plus } from "lucide-react";
-import Pizza from "@/assets/img/pizza-main.png";
-import { Toppings } from "@/components";
-import { Button } from "../ui/button";
+import { PizzaSize, Toppings } from "@/components";
 import { Iproducts } from "@/types";
 import { toppings } from "../list/toppingslist";
 import Image from "next/image";
@@ -10,20 +8,21 @@ const TopingsModal = ({ obj }: { obj: Iproducts }) => {
   return (
     <>
       <div className="w-full flex justify-between items-center ">
-        <div className="flex items-center justify-center left w-1/3 bg-white">
-          <Image src={Pizza} width={150} height={150} alt="pizza is loading" />
+        <div className="flex items-center justify-center left w-1/3 bg-white h-full">
+          <Image
+            src={obj.img}
+            width={150}
+            height={150}
+            alt="pizza is loading"
+          />
         </div>
         <div className="w-2/3 py-3 px-4">
-          <h1 className="font-semibold text-2xl">Margarita</h1>
+          <h1 className="font-semibold text-2xl">{obj.name}</h1>
           <p className="text-muted-foreground mt-2">
             Juicy chicken fillet and crispy bacon combined with signature tomato
             sauce, Mozzarella and onions
           </p>
-          <div className="flex gap-2 mt-5 ">
-            <Button className="rounded-full">S</Button>
-            <Button className="rounded-full bg-white text-black">M</Button>
-            <Button className="rounded-full bg-white text-black">L</Button>
-          </div>
+          <PizzaSize />
           <h1 className="mt-8">Extra Toppings</h1>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-5">
             {toppings.map((ele: Iproducts) => (
