cc5258d51e76451261f40d038de12b13697963ee
nischal1101 <kaflenk123@gmail.com>
2024-09-27 22:24:14 +0545

    fix type error

--- src/components/custom/AllProductCard.tsx
+++ src/components/custom/AllProductCard.tsx
@@ -1,14 +1,14 @@
 "use client";
-import { ICartProduct, Iproducts } from "@/types";
+import { ICartItem, Iproduct } from "@/types";
 import React from "react";
 import ProductCard from "./ProductCard";
 
-const AllProductCard = ({ pizzas }: { pizzas: Iproducts[] }) => {
-  const [cartListItems, setCartListItems] = React.useState<ICartProduct[]>([]);
+const AllProductCard = ({ pizzas }: { pizzas: Iproduct[] }) => {
+  const [cartListItems, setCartListItems] = React.useState<ICartItem[]>([]);
 
   return (
     <div className="grid mt-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 place-items-center">
-      {pizzas.map((pizza: Iproducts) => (
+      {pizzas.map((pizza: Iproduct) => (
         <ProductCard obj={pizza} key={pizza.name} />
       ))}
     </div>
